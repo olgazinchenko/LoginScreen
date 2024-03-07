c563803a242bb9222c3d7fe23cb4b3ecb42f0dba
olgazinchenko <ozinchenko.pm@gmail.com>
2024-03-07 14:02:27 +0100

    Added email and password validation and check

--- LoginScreen/ViewController.swift
+++ LoginScreen/ViewController.swift
@@ -21,28 +21,112 @@ class ViewController: UIViewController {
     @IBOutlet weak var emailIcon: UIImageView!
     @IBOutlet weak var passwordIcon: UIImageView!
     
+    // MARK: - Properties
+    private let activeColor = UIColor.notes
+    private let inactiveColor = UIColor.systemGray5
+    private var email: String = ""
+    private var password: String = ""
+    
+    private let mockEmail = "abc@gmail.com"
+    private let mockPassword = "123456"
+    
     // MARK: - IBActions
     @IBAction func loginButtonAction(_ sender: UIButton) {
-        performSegue(withIdentifier: "goToHomePage", sender: sender)
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        
+        if email.isEmpty {
+            makeErrorField(textField: emailTextField)
+        }
+        
+        if password.isEmpty {
+            makeErrorField(textField: passwordTextField)
+        }
+        
+        if email == mockEmail,
+           password == mockPassword {
+            performSegue(withIdentifier: "goToHomePage", sender: sender)
+        } else {
+            let alert = UIAlertController(title: "Error",
+                                          message: "Wrong email or password",
+                                          preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default)
+            
+            alert.addAction(action)
+            present(alert, animated: true)
+        }
     }
     @IBAction func signupButtonAction(_ sender: UIButton) {
     }
     
-    
-    
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         addShadowToLoginButton()
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        emailTextField.becomeFirstResponder()
     }
     
     // MARK: - Methods
     private func addShadowToLoginButton() {
-        loginButton.layer.shadowColor = UIColor.loginButton.cgColor
+        loginButton.layer.shadowColor = activeColor.cgColor
         loginButton.layer.shadowOffset = CGSize(width: 0, height: 2)
         loginButton.layer.shadowOpacity = 0.7
         loginButton.layer.shadowRadius = 6
     }
+}
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let text = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines), 
+                !text.isEmpty else { return }
+        switch textField {
+        case emailTextField:
+            let isValidEmail = check(email: text)
+            
+            if isValidEmail {
+                email = text
+                emailIcon.tintColor = inactiveColor
+                emailDividerView.backgroundColor = inactiveColor
+            } else {
+                makeErrorField(textField: textField)
+            }
+        case passwordTextField:
+            let isValidPassword = check(password: text)
+            
+            if isValidPassword {
+                password = text
+                passwordIcon.tintColor = inactiveColor
+                passwordDividerView.backgroundColor = inactiveColor
+            } else {
+                makeErrorField(textField: textField)
+            }
+        default:
+            print("Unknown textField")
+        }
+    }
+    
+    private func check(email: String) -> Bool {
+        return email.contains("@") && email.contains(".")
+    }
+    
+    private func check(password: String) -> Bool {
+        return password.count >= 4
+    }
+    
+    private func makeErrorField(textField: UITextField) {
+        switch textField {
+        case emailTextField:
+            emailIcon.tintColor = activeColor
+            emailDividerView.backgroundColor = activeColor
+        case passwordTextField:
+            passwordIcon.tintColor = activeColor
+            passwordDividerView.backgroundColor = activeColor
+        default:
+            print("Unknown textField")
+        }
+    }
 }
 
