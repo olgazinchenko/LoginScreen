eef0f96872628ac21b7a4849c6b7f817e96cb1c1
olgazinchenko <ozinchenko.pm@gmail.com>
2024-03-10 13:24:00 +0100

    Added localization to the application

---
+++ LoginScreen/String+Ex.swift
@@ -0,0 +1,14 @@
+//
+//  String+Ex.swift
+//  LoginScreen
+//
+//  Created by ozinchenko.dev on 07/03/2024.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
--- LoginScreen/ViewController.swift
+++ LoginScreen/ViewController.swift
@@ -24,8 +24,20 @@ class ViewController: UIViewController {
     // MARK: - Properties
     private let activeColor = UIColor.notes
     private let inactiveColor = UIColor.systemGray5
-    private var email: String = ""
-    private var password: String = ""
+    private var email: String = "" {
+        didSet {
+            loginButton.isUserInteractionEnabled = !(email.isEmpty || password.isEmpty)
+            loginButton.backgroundColor = !(email.isEmpty || password.isEmpty) ? activeColor : inactiveColor
+            loginButton.layer.shadowColor = !(email.isEmpty || password.isEmpty) ? activeColor.cgColor : inactiveColor.cgColor
+        }
+    }
+    private var password: String = "" {
+        didSet {
+            loginButton.isUserInteractionEnabled = !(email.isEmpty || password.isEmpty)
+            loginButton.backgroundColor = !(email.isEmpty || password.isEmpty) ? activeColor : inactiveColor
+            loginButton.layer.shadowColor = !(email.isEmpty || password.isEmpty) ? activeColor.cgColor : inactiveColor.cgColor
+        }
+    }
     
     private let mockEmail = "abc@gmail.com"
     private let mockPassword = "123456"
@@ -43,14 +55,14 @@ class ViewController: UIViewController {
             makeErrorField(textField: passwordTextField)
         }
         
-        if email == mockEmail,
+        if email == mockEmail,#imageLiteral(resourceName: "simulator_screenshot_7A182A8C-63B0-4291-94E3-70B34958FE94.png")
            password == mockPassword {
             performSegue(withIdentifier: "goToHomePage", sender: sender)
         } else {
-            let alert = UIAlertController(title: "Error",
-                                          message: "Wrong email or password",
+            let alert = UIAlertController(title: "Error".localized,
+                                          message: "Wrong email or password".localized,
                                           preferredStyle: .alert)
-            let action = UIAlertAction(title: "OK", style: .default)
+            let action = UIAlertAction(title: "OK".localized, style: .default)
             
             alert.addAction(action)
             present(alert, animated: true)
@@ -63,24 +75,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        addShadowToLoginButton()
+        setupLoginButton()
         emailTextField.delegate = self
         passwordTextField.delegate = self
         emailTextField.becomeFirstResponder()
     }
     
     // MARK: - Methods
-    private func addShadowToLoginButton() {
+    private func setupLoginButton() {
+        // Add shadow
         loginButton.layer.shadowColor = activeColor.cgColor
         loginButton.layer.shadowOffset = CGSize(width: 0, height: 2)
         loginButton.layer.shadowOpacity = 0.7
         loginButton.layer.shadowRadius = 6
+        
+        // Make the button inactive
+        loginButton.isUserInteractionEnabled = false
+        loginButton.backgroundColor = inactiveColor
+        loginButton.layer.shadowColor = inactiveColor.cgColor
     }
 }
 
 extension ViewController: UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
-        guard let text = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines), 
+        
+        guard let text = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
                 !text.isEmpty else { return }
         switch textField {
         case emailTextField:
@@ -91,6 +110,7 @@ extension ViewController: UITextFieldDelegate {
                 emailIcon.tintColor = inactiveColor
                 emailDividerView.backgroundColor = inactiveColor
             } else {
+                email = ""
                 makeErrorField(textField: textField)
             }
         case passwordTextField:
@@ -101,6 +121,7 @@ extension ViewController: UITextFieldDelegate {
                 passwordIcon.tintColor = inactiveColor
                 passwordDividerView.backgroundColor = inactiveColor
             } else {
+                password = ""
                 makeErrorField(textField: textField)
             }
         default:
